658bf11a44258c15121b5863b3596a3e03cd80f6
Julius M <jjmanowski@gmail.com>
2023-03-02 11:23:16 +0000

    - checked DB integration by running select statement for city details - also created city class to check above integration

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,6 +12,13 @@ public class App
         // Connect to database
         a.connect();
 
+        //get City
+        String input = "London";
+        City c = a.getCity(input);
+
+        //print city info
+        a.displayCity(c);
+
         // Disconnect from database
         a.disconnect();
     }
@@ -81,4 +88,62 @@ public class App
         }
     }
 
+    /**
+     * method to get a single city's details
+     * @param city_input
+     */
+    public City getCity(String city_input)
+    {
+        //add quotation marks for SQL variable
+        city_input = "'" + city_input + "'";
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.id, city.name, city.countrycode, city.district, city.population "
+                            + "FROM city "
+                            + "WHERE city.name = " + city_input;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                City c = new City();
+                c.city_id = rset.getInt("city.id");
+                c.city_name = rset.getString("city.name");
+                c.city_country_code = rset.getString("city.countrycode");
+                c.city_district = rset.getString("city.district");
+                c.city_population = rset.getInt("city.population");
+                return c;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+        }
+    }
+
+    /**
+     * method to print single city's info
+     * @param c
+     */
+    public void displayCity(City c)
+    {
+        if (c != null)
+        {
+            System.out.println(
+                    "City id: " + c.city_id + "\n"
+                            + "City name: " + c.city_name + "\n"
+                            + "City country code: " + c.city_country_code + "\n"
+                            + "City district: " + c.city_district + "\n"
+                            + "City population: " + c.city_population + "\n");
+        }
+    }
 }
---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,22 @@
+package com.napier.sem;
+
+/**
+ * Represents a city
+ */
+public class City {
+
+    //city id
+    public int city_id;
+
+    //city name
+    public String city_name;
+
+    //city country code
+    public String city_country_code;
+
+    //city district
+    public String city_district;
+
+    //city population
+    public int city_population;
+}
