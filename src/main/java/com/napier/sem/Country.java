0f3ee437236fdd380a9c211826da171340079187
Jules <40277946@live.napier.ac.uk>
2023-03-21 14:16:18 +0000

    making country class

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,22 @@
+package com.napier.sem;
+
+public class Country {
+
+    //country id
+    public String country_id;
+
+    //country name
+    public String country_name;
+
+    //continent
+    public String country_continent;
+
+    //region
+    public String country_region;
+
+    //population
+    public int country_population;
+
+    //capital
+    public int country_capital;
+}
