c7496b11b3ce0875ee4aa382b50d28ff209ef78d
euan-maccoll <40594128@live.napier.ac.uk>
2023-03-23 18:00:48 +0000

    unit tests - needs outputs changed

---
+++ src/main/test/java/mytest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MyTest
+{
+    @Test
+    void unitTest1()
+    {
+        assertEquals(5, 5);
+    }
+    @Test
+    void unitTest2()
+    {
+        assertEquals(5, 5);
+    }
+    @Test
+    void unitTest3()
+    {
+        assertEquals(5, 5);
+    }
+}
