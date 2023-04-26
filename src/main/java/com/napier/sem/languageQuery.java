faa38c1471da00d739f6792d04e8d4bd2331ef68
AleksJakub <124362847+aleksjakub@users.noreply.github.com>
2023-04-26 19:57:51 +0100

    added printlanguages() and getlanguages() methods

--- src/main/java/com/napier/sem/languageQuery.java
+++ src/main/java/com/napier/sem/languageQuery.java
@@ -1,5 +1,54 @@
 package com.napier.sem;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class languageQuery {
 
+    public static void printLanguages(ArrayList<Language> languages)
+    {
+        // Print header
+        System.out.println(String.format("%-30s %-30s %-30s ", "Language", "Population", "Percentage"));
+        // Loop over all countries in the list
+        for (Language l : languages)
+        {
+            String c_string =
+                    String.format("%-30s %-30s %-30s ", l.language, l.total_speakers, l.percentage);
+            System.out.println(c_string);
+        }
+    }
+    public static ArrayList<Language> getLanguages(Connection con) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT cl.language, SUM(c.population * (cl.percentage / 100)) AS total_speakers, ROUND((SUM(c.population * (cl.percentage / 100)) / (SELECT SUM(population) FROM country)) * 100, 2) AS world_percentage "
+                            + "FROM countrylanguage cl "
+                            + "INNER JOIN country c ON c.code = cl.CountryCode "
+                            + "INNER JOIN country cp ON cp.code = cl.CountryCode "
+                            + "WHERE cl.language IN ('Chinese', 'English', 'Hindi', 'Spanish', 'Arabic') "
+                            + "GROUP BY cl.language "
+                            + "ORDER BY total_speakers DESC ";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract city information
+            ArrayList<Language> languages = new ArrayList<Language>();
+            while (rset.next()) {
+                Language l = new Language();
+                l.language = rset.getString("language");
+                l.total_speakers = rset.getString("total_speakers");
+                l.percentage = rset.getString("world_percentage");
+                languages.add(l);
+            }
+            return languages;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get language details");
+            return null;
+        }
+    }
 }
