32e1b28e830a6fba1fec18e7d933c97cd1b4bb1c
jules946 <jjmanowski@gmail.com>
2023-04-27 11:12:56 +0100

    adding 2nd db for testing which has consistent data

--- src/main/java/com/napier/sem/Continent.java
+++ src/main/java/com/napier/sem/Continent.java
@@ -5,11 +5,11 @@ public class Continent {
     // Continent name
     public String continent_name;
     // Continent population
-    public int continent_population;
+    public Long continent_population;
 
     // Continent city population
-    public int continent_city_population;
+    public Long continent_city_population;
 
     // Continent non-city population
-    public int continent_non_city_population;
+    public Long continent_non_city_population;
 }
--- src/test/java/com/napier/sem/BaseTest.java
+++ src/test/java/com/napier/sem/BaseTest.java
@@ -1,12 +1,39 @@
 package com.napier.sem;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class BaseTest {
-    static App app;
+    static Connection con;
 
     @BeforeAll
     static void init() {
-        app = new App();
+        try {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Create a connection object
+            con = DriverManager.getConnection("jdbc:mysql://localhost:33061/testing_world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+            // Get the current database name
+            String dbName = con.getCatalog();
+            System.out.println("Connected to database: " + dbName);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Could not connect to database");
+            System.exit(-1);
+        }
+    }
+
+    @AfterAll
+    static void tearDown() {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println("Error closing connection to database");
+            }
+        }
     }
 }
